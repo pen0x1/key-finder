20826cf479d1ebae9b91c0c43cc28b316f6cf38a
pen0x1 <163335431+pen0x1@users.noreply.github.com>
2024-05-26 13:00:30 +0200

    advance the readability

--- key_recovery.rs
+++ key_recovery.rs
@@ -1,22 +1,21 @@
 mod btc_key_recovery {
     use secp256k1::{Secp256k1, SecretKey};
-    use std::env;
-    use std::str::FromStr;
-    
+    use std::{env, str::FromStr};
+
     pub fn recover_key_from_partial_data(data: &str) -> Option<SecretKey> {
         SecretKey::from_str(data).ok()
     }
-    
+
     pub fn generate_deterministic_key(user_data: &str) -> SecretKey {
         let salt = env::var("KEY_SALT").expect("KEY_SALT must be set");
-        
+
         let hashed_data = format!("{}{}", user_data, salt);
-        
+
         let secp = Secp256k1::new();
-        
-        let secret_key = SecretKey::from_slice(&hashed_data.as_bytes()[0..32])
+
+        let secret_key = SecretKey::from_slice(&hashed_data.as_bytes()[..32])
             .expect("Failed to create SecretKey: Invalid Key Length");
-        
+
         secret_key
     }
 
