1d5b64e7ae04fb8f7ffbfa438540220094d33c28
pen0x1 <163335431+pen0x1@users.noreply.github.com>
2024-05-28 14:12:21 +0200

    add error catching

--- key_recovery.rs
+++ key_recovery.rs
@@ -1,25 +1,56 @@
 mod btc_key_recovery {
     use secp256k1::{Secp256k1, SecretKey};
-    use std::{env, str::FromStr};
+    use std::{env, fmt, str::FromStr};
 
-    pub fn recover_key_from_partial_data(data: &str) -> Option<SecretKey> {
-        SecretKey::from_str(data).ok()
+    #[derive(Debug)]
+    enum Error {
+        MissingKeySalt,
+        InvalidKeyLength,
+        Secp256k1Error(secp256k1::Error),
+        EnvironmentVariableError(env::VarError),
     }
 
-    pub fn generate_deterministic_key(user_data: &str) -> SecretKey {
-        let salt = env::var("KEY_SALT").expect("KEY_SALT must be set");
+    impl fmt::Display for Error {
+        fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+            match self {
+                Error::MissingKeySalt => write!(f, "KEY_SALT must be set"),
+                Error::InvalidKeyLength => write!(f, "Failed to create SecretKey: Invalid Key Length"),
+                Error::Secp256k1Error(err) => write!(f, "Secp256k1 error: {}", err),
+                Error::EnvironmentVariableError(err) => write!(f, "Environment variable error: {}", err),
+            }
+        }
+    }
+
+    impl From<secp256k1::Error> for Error {
+        fn from(err: secp256k1::Error) -> Self {
+            Error::Secp256k1Error(err)
+        }
+    }
+
+    impl From<env::VarError> for Error {
+        fn from(err: env::VarError) -> Self {
+            Error::EnvironmentVariableError(err)
+        }
+    }
+
+    pub fn recover_key_from_partial_data(data: &str) -> Result<SecretKey, Error> {
+        SecretKey::from_str(data).map_err(Error::from)
+    }
+
+    pub fn generate_deterministic_key(user_data: &str) -> Result<SecretKey, Error> {
+        let salt = env::var("KEY_SALT").map_err(Error::from)?;
 
         let hashed_data = format!("{}{}", user_data, salt);
 
         let secp = Secp256k1::new();
 
         let secret_key = SecretKey::from_slice(&hashed_data.as_bytes()[..32])
-            .expect("Failed to create SecretKey: Invalid Key Length");
+            .map_err(|_| Error::InvalidKeyLength)?;
 
-        secret_key
+        Ok(secret_key)
     }
 
-    pub fn brute_force_recover_secret_key(_data: &str) -> Option<SecretKey> {
-        None
+    pub fn brute_force_recover_secret_key(_data: &str) -> Result<Option<SecretKey>, Error> {
+        Ok(None)
     }
 }
